b6b99ccee133c3a2fde70e94331887cc2136b986
mthdht <pharos.labo@gmail.com>
2025-12-09 14:05:26 +0100

    made Role middleware: EnsureHasRole

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\EnsureHasRole;
 
-Route::resource('users', \App\Http\Controllers\Admin\UserController::class)->names('admin.users');
+Route::middleware(['auth', EnsureHasRole::class . ':admin'])
+     ->prefix('admin')
+     ->name('admin.')
+     ->group(function () {
+         Route::resource('users', UserController::class);
+        //  Route::resource('daycares', DaycareController::class);
+        //  Route::resource('messages', MessageController::class);
+     });
 // Route::resource('roles', \App\Http\Controllers\Admin\RoleController::class)->names('admin.roles');
 // Route::resource('permissions', \App\Http\Controllers\Admin\PermissionController::class)->names('admin.permissions');
 // Route::resource('settings', \App\Http\Controllers\Admin\SettingController::class)->only(['index', 'update'])->names('admin.settings');
