c50ad444e8c791f83040bbf8d8d2da24ffb5b03e
mthdht <pharos.labo@gmail.com>
2025-12-10 08:02:07 +0100

    made seeder for users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->withoutTwoFactor()->create([
-            'name' => 'mth',
-            'email' => 'mthdht@gmail.com',
-        ]);
+        $this->call(RolePermissionSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create Super Admin
+        $admin = User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'admin@daycare.test',
+            'email_verified_at' => now(),
+        ]);
+        $admin->assignRole('admin');
+
+        // Create test directors
+        $director1 = User::factory()->create([
+            'name' => 'Jean Dupont',
+            'email' => 'director1@daycare.test',
+            'email_verified_at' => now(),
+        ]);
+        $director1->assignRole('director');
+
+        $director2 = User::factory()->create([
+            'name' => 'Marie Martin',
+            'email' => 'director2@daycare.test',
+            'email_verified_at' => now(),
+        ]);
+        $director2->assignRole('director');
+
+        // Create test staff
+        $staff1 = User::factory()->create([
+            'name' => 'Sophie Bernard',
+            'email' => 'staff1@daycare.test',
+            'email_verified_at' => now(),
+        ]);
+        $staff1->assignRole('staff');
+
+        $staff2 = User::factory()->create([
+            'name' => 'Lucas Petit',
+            'email' => 'staff2@daycare.test',
+            'email_verified_at' => now(),
+        ]);
+        $staff2->assignRole('staff');
+
+        // Create test parents
+        $parent1 = User::factory()->create([
+            'name' => 'Emma Dubois',
+            'email' => 'parent1@daycare.test',
+            'email_verified_at' => now(),
+        ]);
+        $parent1->assignRole('parent');
+
+        $parent2 = User::factory()->create([
+            'name' => 'Thomas Robert',
+            'email' => 'parent2@daycare.test',
+            'email_verified_at' => now(),
+        ]);
+        $parent2->assignRole('parent');
+
+        $this->command->info('Admin and test users created successfully!');
+        $this->command->info('Admin: admin@daycare.test / password');
+    }
+}
