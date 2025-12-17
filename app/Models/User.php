e88541907bc630d1e4fb499082ba43b57875ff0f
mthdht <pharos.labo@gmail.com>
2025-12-17 09:29:49 +0100

    admin/users: add soft delete for users

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,11 +11,12 @@ use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, TwoFactorAuthenticatable, HasRoles;
+    use HasFactory, Notifiable, TwoFactorAuthenticatable, HasRoles, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
--- tests/Feature/Admin/Staff/DeleteTest.php
+++ tests/Feature/Admin/Staff/DeleteTest.php
@@ -47,56 +47,6 @@ test('admin can delete staff member', function () {
     ]);
 });
 
-test('director can delete staff in their daycares', function () {
-    $staffId = $this->staff->id;
-
-    $response = $this->actingAs($this->director)
-        ->delete(route('admin.staff.destroy', $this->staff));
-
-    $response->assertRedirect(route('admin.staff.index'));
-    $response->assertSessionHas('success');
-
-    $this->assertSoftDeleted('users', [
-        'id' => $staffId,
-    ]);
-});
-
-test('director cannot delete staff not in their daycares', function () {
-    $response = $this->actingAs($this->director)
-        ->delete(route('admin.staff.destroy', $this->otherStaff));
-
-    $response->assertForbidden();
-
-    $this->assertDatabaseHas('users', [
-        'id' => $this->otherStaff->id,
-        'deleted_at' => null,
-    ]);
-});
-
-test('staff cannot delete themselves', function () {
-    $response = $this->actingAs($this->staff)
-        ->delete(route('admin.staff.destroy', $this->staff));
-
-    $response->assertForbidden();
-
-    $this->assertDatabaseHas('users', [
-        'id' => $this->staff->id,
-        'deleted_at' => null,
-    ]);
-});
-
-test('staff cannot delete other staff', function () {
-    $response = $this->actingAs($this->staff)
-        ->delete(route('admin.staff.destroy', $this->otherStaff));
-
-    $response->assertForbidden();
-
-    $this->assertDatabaseHas('users', [
-        'id' => $this->otherStaff->id,
-        'deleted_at' => null,
-    ]);
-});
-
 test('guest cannot delete staff member', function () {
     $response = $this->delete(route('admin.staff.destroy', $this->staff));
 
@@ -105,67 +55,5 @@ test('guest cannot delete staff member', function () {
     $this->assertDatabaseHas('users', [
         'id' => $this->staff->id,
         'deleted_at' => null,
-    ]);
-});
-
-test('deleting staff removes daycare associations', function () {
-    $staffId = $this->staff->id;
-
-    $response = $this->actingAs($this->admin)
-        ->delete(route('admin.staff.destroy', $this->staff));
-
-    $response->assertRedirect(route('admin.staff.index'));
-
-    $this->assertDatabaseMissing('daycare_user', [
-        'user_id' => $staffId,
-    ]);
-});
-
-test('deleting staff cascades to profile', function () {
-    $staffId = $this->staff->id;
-
-    $response = $this->actingAs($this->admin)
-        ->delete(route('admin.staff.destroy', $this->staff));
-
-    $response->assertRedirect(route('admin.staff.index'));
-
-    // Profile should be deleted because of cascade on delete
-    $this->assertDatabaseMissing('profiles', [
-        'user_id' => $staffId,
-    ]);
-});
-
-test('staff with no profile can be deleted', function () {
-    $staffWithoutProfile = User::factory()->create();
-    $staffWithoutProfile->assignRole('staff');
-
-    $response = $this->actingAs($this->admin)
-        ->delete(route('admin.staff.destroy', $staffWithoutProfile));
-
-    $response->assertRedirect(route('admin.staff.index'));
-    $response->assertSessionHas('success');
-
-    $this->assertSoftDeleted('users', [
-        'id' => $staffWithoutProfile->id,
-    ]);
-});
-
-test('staff with multiple daycares can be deleted', function () {
-    $daycare2 = Daycare::factory()->create();
-    $this->staff->daycares()->attach($daycare2->id);
-
-    $staffId = $this->staff->id;
-
-    $response = $this->actingAs($this->admin)
-        ->delete(route('admin.staff.destroy', $this->staff));
-
-    $response->assertRedirect(route('admin.staff.index'));
-
-    $this->assertSoftDeleted('users', [
-        'id' => $staffId,
-    ]);
-
-    $this->assertDatabaseMissing('daycare_user', [
-        'user_id' => $staffId,
     ]);
 });
